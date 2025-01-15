adc47e08f15fee13691c4fe76af595d1b53f2683
Reinaldo Landaeta <144262402+REyyyy43@users.noreply.github.com>
2025-01-15 16:00:01 -0400

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+
+import { Amplify } from 'aws-amplify';
+import App from './App.tsx';
+import outputs from '../amplify_outputs (2).json';
+import './index.css';
+
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+    
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+
+);

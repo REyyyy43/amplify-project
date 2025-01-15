2971bf0f190e3fa81a8440f102193ac5607c9531
Reinaldo Landaeta <144262402+REyyyy43@users.noreply.github.com>
2025-01-15 15:25:27 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { Authenticator } from '@aws-amplify/ui-react';
-
 import { Amplify } from 'aws-amplify';
-import App from './App.tsx';
-import outputs from '../amplify_outputs (2).json';
-import './index.css';
-
+import outputs from '../amplify_outputs.json'; // Asegúrate de que la ruta esté correcta
 import '@aws-amplify/ui-react/styles.css';
+import TenantAdminDashboard from './pages/tenantAdmin/DashboardTenant'; // Ruta correcta
 
+// Configuración de Amplify
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Punto de entrada principal
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-    
-  <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </React.StrictMode>
-
+root.render(
+  <TenantAdminDashboard />
 );
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Incluye tus componentes
+    "./src/styles/tailwind.css", // Opcional, pero bueno para asegurarse
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

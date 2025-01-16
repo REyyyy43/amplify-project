e24f878aa0d5048be53de53b6a0f013ca18cdf1b
Goel <g.vazquez.d@gmail.com>
2025-01-16 15:25:02 +0100

    App tab update

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import UnitsView from './components/units/UnitsView';
 import BuildingsView from './components/buildings/BuildingsView';
 import MaintenanceRequestsView from './components/maintenance/MaintenanceRequestsView';
 import MessagesView from './components/messages/MessagesView';
+import { User } from 'aws-cdk-lib/aws-iam';
 
 const TenantAdminDashboard = () => {
   const { signOut } = useAuthenticator();
@@ -23,13 +24,11 @@ const TenantAdminDashboard = () => {
         <nav className="sidebar-nav">
           <a onClick={() => handleNavigation("dashboard")} className="sidebar-item">Dashboard Overview</a>
           <a onClick={() => handleNavigation("buildings")} className="sidebar-item">Buildings</a>
-          <a onClick={() => handleNavigation("users")} className="sidebar-item">Users</a>
           <a onClick={() => handleNavigation("units")} className="sidebar-item">Units</a>
+          <a onClick={() => handleNavigation("users")} className="sidebar-item">Users</a>
           <a onClick={() => handleNavigation("task")} className="sidebar-item">Task</a>
-          <a onClick={() => handleNavigation("messages")} className="sidebar-item">Messages</a>
-          <a onClick={() => handleNavigation("notifications")} className="sidebar-item">Notifications</a>
           <a onClick={() => handleNavigation("maintenance")} className="sidebar-item">Maintenance Requests</a>
-          <a onClick={() => handleNavigation("reports")} className="sidebar-item">Reports & Analytics</a>
+          <a onClick={() => handleNavigation("messages")} className="sidebar-item">Messages</a>
         </nav>
       </aside>
 
@@ -38,7 +37,7 @@ const TenantAdminDashboard = () => {
         {/* Header */}
         <header className="header">
           <div className="header-title-search">
-            <h1 className="header-title">Tenant Admin Dashboard</h1>
+            <h1 className="header-title">Tenant Admin Dashboard : {User.name}</h1>
           </div>
           <button onClick={signOut} className="sign-out-btn">Sign Out</button>
         </header>
@@ -52,12 +51,12 @@ const TenantAdminDashboard = () => {
             </section>
           )}
 
-          {currentView === "users" && <UsersView />}
           {currentView === "buildings" && <BuildingsView />}
           {currentView === "units" && <UnitsView />}
+          {currentView === "users" && <UsersView />}
           {currentView === "task" && <TaskView />}
-          {currentView === "messages" && <MessagesView />}
           {currentView === "maintenance" && <MaintenanceRequestsView />}
+          {currentView === "messages" && <MessagesView />}
         </main>
       </div>
     </div>
